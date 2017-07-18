e6b7f3b8f6cc6ce80f671afcb9851d6d352ecb8b
minho kim <01051538537a@likelion.org>
2017-07-18 07:05:30 +0000

    crawling youtube

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,45 @@
 require 'open-uri'
 
+# encoding: utf-8
 class HomeController < ApplicationController
+  # encoding: utf-8
   def index
       @titles =Array.new
       @hrefs =Array.new
+     
+      
     uri= 'https://www.youtube.com/channel/UC8oSz7z588lRvzmUhLMajsQ/videos'
-    doc = Nokogiri::HTML(open(uri))
-
+    doc = Nokogiri::XML(open(uri))
    
     doc.css('.yt-lockup-title').each do |x|
         @titles << x.inner_text
         end    
         
         doc.css('.yt-lockup-title a').each do |d|
-        @hrefs  << d
+        @hrefs  << d.attr('href')    # a태크 href 따오는법!!
       end
-      
     
     
+  end
+  
+  
+  def result
+    
+     @searchresult =Array.new
+     @r_hrefs= Array.new
+     
+    @searchmovie = CGI::escape(params[:input_search])
+    searchurl= "https://www.youtube.com/results?search_query=#{@searchmovie}"
+    searchdata = Nokogiri::XML(open(searchurl))
+    
+     searchdata.css('.yt-lockup-title').each do |r|
+       @searchresult << r.inner_text
+  end
+  
+  searchdata.css('.yt-lockup-title a').each do |a|
+    @r_hrefs << a.attr('href')
+  end
+  
   end
 end
 
